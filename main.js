97a89a7648464b6585349bed9deeca21f0c2a1e2
AidenSPS <aidenkusmierczak2016@gmail.com>
2023-01-18 13:59:07 -0500

    addedJokes

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 //Global Variables
+var knockArray = [
+[("Chris", "Christmas is coming!")], 
+[("Boo", "Why are you crying? ")],
+["Broken Pencil, Nevermind, it's pointless"],
+["Nobel, Nobel…that’s why I knocked!"],
+["Aunt Jemima, I'm glad I ain't joe mamma"],
+["AXOLTL, I AXOLOTL Questions"],
+["Doctor, No, no, just the doctor."],
+["Oink oink, Make up your mind are you a pig or an owl?"],
+["Tank, You're welcome."],["Sue, I'll see you in court"]
+];
+
+var punArray = [ //In the Fosters, the "Outlaw because they are INlaws"
+[],
+[]
+];
