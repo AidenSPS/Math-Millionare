0e1fee8908ea44ce87892cc5b51fad9c244d6427
AidenSPS <aidenkusmierczak2016@gmail.com>
2023-01-19 10:54:22 -0500

    First Grade Done

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
 //Global Variables
-var knockArray = [
-[("Chris", "Christmas is coming!")], 
-[("Boo", "Why are you crying? ")],
-["Broken Pencil, Nevermind, it's pointless"],
-["Nobel, Nobel…that’s why I knocked!"],
-["Aunt Jemima, I'm glad I ain't joe mamma"],
-["AXOLTL, I AXOLOTL Questions"],
-["Doctor, No, no, just the doctor."],
-["Oink oink, Make up your mind are you a pig or an owl?"],
-["Tank, You're welcome."],["Sue, I'll see you in court"]
-];
+var cashCounter = 0; //Displayed, going to have table which with correct questions highlighted
+var questionCounter = 0; //Hidden counter that will be translated to dollars to represent cashCounter
 
-var punArray = [ //In the Fosters, the "Outlaw because they are INlaws"
-[],
-[]
-];
+//Question Arrays (needed for randomization)
+var gradeOne = [[["I have 5 cookies, I ate 2, How many do I have left?"],["3"]],[["I had 10 dollars, I was given 5 in allowance, how many dollars do I have?"],["15"]],[["What is 5 + 5?"],["10"]],[["What is 5-5?"],["0"]],[["When creating this the time is 10:50AM, I spent fifty minutes and I will spend an hour and a half making this game, how much time will I spend making this game?"],["40 Minutes"]]]; //[Main] -->[Ranomized Question] -->[Question] --> [Answer]
+
+var gradeTwo = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var gradeThree = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var gradeFour = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var gradeFive = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var gradeSix = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var gradeSeven = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var preAlgebra = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var algebra1 = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var geometry = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var algebra2 = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var preCalculus = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var statistics = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var bonus1 = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+var bonus2 = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+
+
+//Functions and Local Variables
+
+
+
+
+
+
+
+
+
+//DOM Scripts
