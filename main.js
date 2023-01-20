4dfcf0cd156890ca8b1badd465dd6390866ca611
AidenSPS <aidenkusmierczak2016@gmail.com>
2023-01-20 10:23:50 -0500

    Grade 2 and 3 Finished

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ var questionCounter = 0; //Hidden counter that will be translated to dollars to 
 //Question Arrays (needed for randomization)
 var gradeOne = [[["I have 5 cookies, I ate 2, How many do I have left?"],["3"]],[["I had 10 dollars, I was given 5 in allowance, how many dollars do I have?"],["15"]],[["What is 5 + 5?"],["10"]],[["What is 5-5?"],["0"]],[["When creating this the time is 10:50AM, I spent fifty minutes and I will spend an hour and a half making this game, how much time will I spend making this game?"],["40 Minutes"]]]; //[Main] -->[Ranomized Question] -->[Question] --> [Answer]
 
-var gradeTwo = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+var gradeTwo = [[["I have a sequence of numbers, (2,4,6,8,10,12), what is the next number in the sequence?"],["14"]],[["I have a sequence of numbers, (100,90,80,70), what is the next number in the sequence?"],["60"]],[["I have 5 dollars to start with, each week I get 5 more dollars, how many dollars will I have after 7 weeks?"],["35 Dollars"]],[["I have 100 dollars, I then split it among my ten best friends, how much money did each person, including myself recieve after the split?"],["10"]],[["What is 1/2 + 5/10?"],["1"]]];
 
-var gradeThree = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+var gradeThree = [[["I bought a piece of land recently with 5 acre by 7 acre dimentions. What is the Area and Perimeter of my land in acres? Put answer with a / and with no spaces"],["35/24"]],[["I have two shapes, I have a right triangle and an equalateral triangle, what does the right triangle have that the equalateral triangle doesn't? Put your in 'A (x)(measurement) angle' "],["A 90 degree angle"]],[["I have a right triangle with a height of 4 and a base of 5, what is the Area of the Triangle?"],["10 Units"]],[["What is 7/3 as a mixed number? Put your answer in (x) (y)/(z) format (ie. 5 1/5)"],["2 1/3"]],[["What is 4 1/3 as an improper fraction?"],["13/3"]]];
 
 var gradeFour = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
 
