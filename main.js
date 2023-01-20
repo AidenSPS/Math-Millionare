e9a3c00387714463a99cb336f4325c85b116adaf
AidenSPS <aidenkusmierczak2016@gmail.com>
2023-01-20 14:13:29 -0500

    Missing Statistics + Bonus 2
    Also REMEMBER: FIGURE OUT THAT PROBLEM COMMENTED ON ALGEBRA 2

--- main.js
+++ main.js
@@ -15,21 +15,21 @@ var gradeFive = [[["How would you express the sum of 508 and x?"],["508 + x"]],[
 
 var gradeSix = [[["I have 3 green balls, I then put it in a bag with 5 purple balls. What is the ratio between GREEN BALLS and PURPLE BALLS? Write in (x):(y)"],["3:5"]],[["What are the whole number factors of 50? Write factors within commas (ie: 10 = 1,2,5,10)"],["1,2,5,10,25,50"]],[["what is 50% of 5000"],["2500"]],[["Compute: (4/2)/(2)"],["1"]],[["What shape does a 45/45/45 represent?"],["Equalateral Triangle"]]];
 
-var gradeSeven = [[["Compute: f(x) = 1/2x + 10. What is f(5)"],["12.5"]],[["What is 3/4 as a percent?"],["75%"]],[[],[]],[[],[]],[[],[]]];
+var gradeSeven = [[["Compute: f(x) = 1/2x + 10. What is f(5)"],["12.5"]],[["What is 3/4 as a percent?"],["75%"]],[["Compute for x: y = x + 14; when y = 43. Write in x = (answer)"],["x = 29"]],[["I have a 30/60/90 triangle. Solve for the hypotenuse or x, given the following numbers. 3/4/x"],["5"]],[["What is the square root of 25?"],["5"]]];
 
-var preAlgebra = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+var preAlgebra = [[["Solve for x given the quadratic; (x^2 * 5 + 1)"],["5"]],[["If I have an ordered pair, (1,1), (2,1), (2,2); Is this a function? Answer with Yes or No"],["No"]],[["Solve the following cubic: x^3 when x = 2"],["8"]],[["Is the following sequence Arithmetic or Geometric: (2,4,6,8,10)"],["Arithmetic"]],[["I have a 45/45/45 Triangle, one of my side lengths is 10 units? What are the side lengths of the other two side lengths?"],["10"]]];
 
-var algebra1 = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+var algebra1 = [[["Using Equation Manipulation, what does i equal when y = print? Solve using i ="],["i = y/prnt"]],[["Using your knowledge of polynomials, what is the highest degree of the following polynomial. x^6 + x^3 + x"],["6"]],[["What is the cube root of 64?"],["4"]],[["When points (2,1) and (0,7) are plotted on a real plain, where is the midpoint with respect to the y-axis?"],["x = 1"]],[["What is the absolute value of 46?"],["46"]]];
 
-var geometry = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+var geometry = [[["Using trigonometry, find sin(60)? Round to the nearest 10th"],["−0.3"]],[["Prove why a 30/60/90 is a right triangle?"],["It has a 90 degree angle"]],[["If f(x) = 5x + 8. And f(x) is a parent function, describe the transformation: f'(x) = 5x + 10?"],["Translation of 2 units to the right"]],[["What trigonometric function is used to solve for the hypotenuse?"],["tan(x)"]],[["What are the three sides of a triangle when in relation to trigonometry called? List with commas and Capital letters (ie Fifty, Fourty, Thirty)."],["Adjacent, Opposite, Hypotenuse"]]];
 
-var algebra2 = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+var algebra2 = [[["Solve the polynomial: y = 10x^3 - 5x^2 - 3x + 486"],[/* Solve for this on paper */]],[["What does log(10) equal?"],["1"]],[["Solve for x: y = log_10(10^x)"],["x"]],[["True or False: ln(x) and log_10(x) are the same? Answer with True or False as written here."],["False"]],[["What is the approximate value of e? Round to the nearest 10th place"],["2.7"]]];
 
-var preCalculus = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+var preCalculus = [[["Compute: 5/0"],["undefined"]],[["What is the formula for any and all power functions?"],["y = kx^p"]],[["Find the hole in the following function: (7x + 3)(x-8)/(x-8)"],["8"]],[["If I have a trigonometic function of f(x) = sin(x), f'(x) would equal? Put f'(x) before your answer"],["f'(x) = sin^-1(x)"]],[["What is the amplitude of f(x) if the local minimum is 12 and the local minimum is 2 with a domain restriction of -5 <= x <= 15?"],["6"]]];
 
 var statistics = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
 
-var bonus1 = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
+var bonus1 = [[["On a real xy plane, does y = function(x)?"],["Yes"]],[["Compute: 5 % 5 =?"],["0"]],[["I have a polynomial, x^10 + x = f(x); How many zeros are in my function?"],["10"]],[["Compute from Binary to Integer: 11101001 + 11111011"],["484"]],[["Compute from Integer to Binary: 40 - 25 = ?"],["1111"]]];
 
 var bonus2 = [[[],[]],[[],[]],[[],[]],[[],[]],[[],[]]];
 
